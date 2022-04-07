1d404f1f2fee02c7b44a83a3719e8cf315a71e71
maykodo <mayko_90@hotmail.com>
2022-04-07 07:46:19 -0300

    atualização de pagina

---
+++ projetos/index3.php
@@ -0,0 +1,13 @@
+<html>
+    <header>
+        <title> Meu Primeiro site em PHP </title>
+    </header>
+
+    <body> 
+        <?php
+            for ( $i = 0 ; $i < 10; $i++) {
+                print ("Linha numero" . $i . "<br />");
+            }
+        ?>
+    </body>
+</html>
